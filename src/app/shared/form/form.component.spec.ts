31317b0501394f375761b9af4373a7184dc0e4a4
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-06-29 22:49:44 +0200

    Add FormComponent test

--- src/app/shared/form/form.component.spec.ts
+++ src/app/shared/form/form.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormComponent } from './form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FormComponent', () => {
   let component: FormComponent;
@@ -8,7 +13,14 @@ describe('FormComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FormComponent]
+      declarations: [FormComponent],
+      imports: [
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+      ],
     });
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
@@ -18,4 +30,42 @@ describe('FormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add a new row when addRow is executed', () => {
+    component.addRow();
+    expect(component.orderForm.length).toBe(1);
+  });
+
+  it('should remove a row by indicating it indexes and confirm the deleting', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+
+    component.addRow();
+    component.deleteRow(0);
+
+    expect(component.orderForm.length).toBe(0);
+  });
+
+  it('should not remove a row if confirm no button is clicked', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+
+    component.addRow();
+    component.deleteRow(0);
+
+    expect(component.orderForm.length).not.toBe(0);
+  });
+
+  it('should populate FormArray with mockData', () => {
+    component.prepopulateForm();
+
+    expect(component.orderForm.length).toBe(4);
+  });
+
+  it('should run the prepopulate once', () => {
+    spyOn(component.orderForm, 'push')
+
+    component.prepopulateForm();
+    component.prepopulateForm();
+
+    expect(component.orderForm.push).toHaveBeenCalledTimes(4)
+  })
 });
